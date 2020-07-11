623eefc1a3b09f5e90417c4882df77b56e750200
Alexandr <ar.mandryka@gmail.com>
2020-07-11 23:34:49 +0300

    Updates

---
+++ precache-manifest.7f1a92d72e15e312b3f4ce06254be13c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e4a3599649859707f1fc34fd89e467e",
+    "url": "/blynk-io/index.html"
+  },
+  {
+    "revision": "8d977bbd24895374bc68",
+    "url": "/blynk-io/static/css/main.d95e2501.chunk.css"
+  },
+  {
+    "revision": "29c6206b765156f7a81c",
+    "url": "/blynk-io/static/js/2.902e8e04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/blynk-io/static/js/2.902e8e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d977bbd24895374bc68",
+    "url": "/blynk-io/static/js/main.12a6b9a2.chunk.js"
+  },
+  {
+    "revision": "29e0305119f2fb767003",
+    "url": "/blynk-io/static/js/runtime-main.06b07a56.js"
+  }
+]);
